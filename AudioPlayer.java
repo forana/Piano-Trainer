b2c50e6c083889ee6ef0160903f901b4c3921018
forana <none@none>
2010-11-01 23:12:59 -0500

    Case 1676 Finished anticipated skeletons, stage is set for AudioPlayer to be worked on.

--- AudioPlayer.java
+++ AudioPlayer.java
@@ -18,5 +18,6 @@ public class AudioPlayer
 	
 	public AudioPlayer(SongModel songModel)
 	{
+		
 	}
 }
--- SongPlayer.java
+++ SongPlayer.java
@@ -2,14 +2,33 @@ package crescendo.base;
 
 /**
  * A SongPlayer does the actual running through the song and propagation of events.
+ * 
  * @author forana
- *
  */
 public class SongPlayer
 {
+	// TODO Pretty much all of this class
+	
+	/**
+	 * The songed that is being played.
+	 */
 	private SongModel songModel;
 	
+	/**
+	 * TODO This
+	 * 
+	 * @param songModel
+	 */
 	public SongPlayer(SongModel songModel)
 	{
+		this.songModel=songModel;
+	}
+	
+	/**
+	 * TODO This
+	 */
+	public SongModel getSong()
+	{
+		return this.songModel;
 	}
 }

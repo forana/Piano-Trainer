257ca7effcbca5e181985bc63fbc3b220067ac5f
forana <none@none>
2010-11-02 19:09:40 -0500

    Case 1022 Adding MidiEvent, waiting on Corey to edit the EventDispatcher.

---
+++ MidiEvent.java
@@ -0,0 +1,84 @@
+package crescendo.base;
+
+/**
+ * MidiEvent encapsulates a musical note, the action involved, and the velocity, as well as the time at which the event occurred.
+ * 
+ * @author forana
+ */
+public class MidiEvent
+{
+	/**
+	 * The note involved.
+	 */
+	private int note;
+	
+	/**
+	 * The velocity of the note.
+	 */
+	private double velocity;
+	
+	/**
+	 * The time in milliseconds at which this event occurred.
+	 */
+	private long timestamp;
+	
+	/**
+	 * The action associated with this event.
+	 */
+	private NoteAction action;
+	
+	/**
+	 * Creates a new MidiEvent.
+	 * 
+	 * @param note The MIDI note associated with this event.
+	 * @param velocity The velocity of the note.
+	 * @param action The action associated with this event.
+	 */
+	public MidiEvent(int note,double velocity,NoteAction action)
+	{
+		this.note=note;
+		this.velocity=velocity;
+		this.timestamp=System.currentTimeMillis();
+		this.action=action;
+	}
+	
+	/**
+	 * The note associated with this event.
+	 * 
+	 * @return The note associated with this event.
+	 */
+	public int getNote()
+	{
+		return this.note;
+	}
+	
+	/**
+	 * The velocity at which the note was played.
+	 * 
+	 * @return The velocity at which the note was played.
+	 */
+	public double getVelocity()
+	{
+		return this.velocity;
+	}
+	
+	/**
+	 * The time at which this event occurred, in milliseconds.
+	 * 
+	 * @return The time at which this event occurred, in milliseconds since the unix epoch.
+	 */
+	public long getTimestamp()
+	{
+		return this.timestamp;
+	}
+	
+	/**
+	 * The action associated with this event.
+	 * 
+	 * @return The action associated with this event.
+	 */
+	public NoteAction getAction()
+	{
+		return this.action;
+	}
+}

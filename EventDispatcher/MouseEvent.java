f6a520a768e9eead39ac7f4bb8d48db9ac5b6767
groszc <none@none>
2010-12-09 01:09:28 -0600

    case 1899,1900,1901: Updated the EventDispatcher, created the ProfileManger internal data structures, and implemented some of the profile selection/creation in the Gen App UI Framework.

---
+++ EventDispatcher/MouseEvent.java
@@ -0,0 +1,50 @@
+package crescendo.base.EventDispatcher;
+
+
+
+/**
+ * MouseEvent
+ * 
+ * 
+ * 
+ * 
+ * @author groszc
+ *
+ */
+public class MouseEvent extends InputEvent{
+	
+	/** The mouse button this event is accociated with. **/ 
+	private int button;
+	
+	/** Any modifiers to this key (shift, strl, alt) **/
+	private Modifier modifier;
+	
+	/** The x-axis the mouse event occurred at. **/
+	private int x;
+	
+	/** The y-axis the mouse event occurred at. **/
+	private int y;
+	
+	
+	/**
+	 * MouseEvent
+	 * 
+	 * 	Default constructor
+	 * 
+	 * @param a - action type
+	 * @param t - timestamp 
+	 * @param i - input type
+	 * @param b - button
+	 * @param x - x axis
+	 * @param y - y axis
+	 * @param m - modifiers
+	 */
+	public MouseEvent(ActionType a, long t, InputType i, int b, int x, int y, Modifier m) 
+	{
+		super(a, t, i);
+		button = b;
+		this.x = x;
+		this.y = y;
+		modifier = m;
+	}
+}

a3171d6d83be27e0309d81f10e94960a01fb4645
forana <none@none>
2010-12-11 14:00:33 -0600

    Case 2146 Implemented and integrated MidiInputStream.

---
+++ parsing/MidiInputStream.java
@@ -0,0 +1,83 @@
+package crescendo.base.parsing;
+
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * Provides an InputStream wrapper that provides a few extra methods used in
+ * Midi file parsing.
+ * @author forana
+ */
+public class MidiInputStream
+{
+	private InputStream stream;
+	private int lastVariableLength;
+	
+	public MidiInputStream(InputStream stream)
+	{
+		this.stream=stream;
+		this.lastVariableLength=0;
+	}
+	
+	public int read() throws IOException
+	{
+		return stream.read();
+	}
+	
+	public void close() throws IOException
+	{
+		stream.close();
+	}
+	
+	/** Reads a variable-width value from a stream. */
+	// side note: variable-width values are reportedly never more than 4 bytes
+	// java int = 32-bit, so int should theoretically be fine for storage
+	public int readVariableWidth() throws IOException
+	{
+		int value=0;
+		int msig;
+		int res;
+		lastVariableLength=0;
+		
+		do
+		{
+			int b=stream.read();
+			msig=b & 0x80;
+			res=b & 0x7F;
+			value=value<<7;
+			value+=res;
+			lastVariableLength++;
+		} while (msig!=0);
+		
+		return value;
+	}
+	
+	/** Returns the length of the last read variable-width number in bytes. **/
+	public int lastVariableLength()
+	{
+		return this.lastVariableLength;
+	}
+	
+	/** Reads an n-byte value from the stream. */
+	public int readBytes(int num) throws IOException
+	{
+		int result=0;
+		for (int i=0; i<num; i++)
+		{
+			result=result<<8;
+			result+=stream.read();
+		}
+		return result;
+	}
+	
+	/** Reads a string of length n from the stream. */
+	public String readString(int n) throws IOException
+	{
+		StringBuilder res=new StringBuilder();
+		for (int i=0; i<n; i++)
+		{
+			res.append((char)stream.read());
+		}
+		return res.toString();
+	}
+}
--- parsing/MidiParser.java
+++ parsing/MidiParser.java
@@ -1,7 +1,6 @@
 package crescendo.base.parsing;
 
 import java.io.File;
-import java.io.InputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,12 +14,9 @@ import crescendo.base.song.Creator;
 
 public class MidiParser implements SongFileParser
 {
-	// the byte-length of whatever the last variable-length variable was
-	private static int lastVariableLength=0;
-	
 	public SongModel parse(File file) throws IOException
 	{
-		InputStream stream=new FileInputStream(file);
+		MidiInputStream stream=new MidiInputStream(new FileInputStream(file));
 		
 		// initialize track items - these cannot be track objects yet as they need to be built
 		List<List<Note>> trackNotes; // this should REALLY be an array, but java can't do that
@@ -37,20 +33,20 @@ public class MidiParser implements SongFileParser
 		
 		// according to specification at http://www.sonicspot.com/guide/midifiles.html
 		// main header
-		if (readBytes(stream,4)!=0x4D546864) // MThd
+		if (stream.readBytes(4)!=0x4D546864) // MThd
 		{
 			throw new IOException("Missing header identifier");
 		}
-		if (readBytes(stream,4)!=6) // always 6
+		if (stream.readBytes(4)!=6) // always 6
 		{
 			throw new IOException("Improper header chunk size");
 		}
-		int formatType=readBytes(stream,2);
+		int formatType=stream.readBytes(2);
 		if (formatType!=0 && formatType!=1)
 		{
 			throw new IOException("MIDI type not accepted in this application");
 		}
-		int numTracks=readBytes(stream,2);
+		int numTracks=stream.readBytes(2);
 		trackNotes=new ArrayList<List<Note>>(numTracks);
 		trackNames=new String[numTracks];
 		trackVoices=new int[numTracks];
@@ -60,7 +56,7 @@ public class MidiParser implements SongFileParser
 			trackNames[i]="";
 			trackVoices[i]=0;
 		}
-		int timeDivisor=readBytes(stream,2);
+		int timeDivisor=stream.readBytes(2);
 		// mode zero means that the divisor is ticks per beat (time is always 4/4)
 		// mode one means that the divisor's top 7 bits is the number of frames per second
 		//  and the bottom 8 bits are the number of ticks per frame (subdivisions)
@@ -75,16 +71,16 @@ public class MidiParser implements SongFileParser
 		// each track header
 		for (int i=0; i<numTracks; i++)
 		{
-			if (readBytes(stream,4)!=0x4D54726B) // MTrk
+			if (stream.readBytes(4)!=0x4D54726B) // MTrk
 			{
 				throw new IOException("Track header not found");
 			}
-			int trackLength=readBytes(stream,4);
+			int trackLength=stream.readBytes(4);
 			int bytesRead=0;
 			while (bytesRead<trackLength)
 			{
-				int deltaTime=readVariableWidth(stream);
-				bytesRead+=lastVariableLength;
+				int deltaTime=stream.readVariableWidth();
+				bytesRead+=stream.lastVariableLength();
 				int opcode=stream.read();
 				int channel=opcode & 0x0F;
 				bytesRead++;
@@ -131,38 +127,38 @@ public class MidiParser implements SongFileParser
 						{
 							int metaCode=stream.read();
 							bytesRead++;
-							int metaLength=readVariableWidth(stream);
-							bytesRead+=lastVariableLength;
+							int metaLength=stream.readVariableWidth();
+							bytesRead+=stream.lastVariableLength();
 							// this should probably not be done here (rather in each branch), but lazyness
 							bytesRead+=metaLength;
 							switch (metaCode)
 							{
 								case 0x00: // Sequence number
-									int sequenceNumber=readBytes(stream,2);
+									int sequenceNumber=stream.readBytes(2);
 									break;
 								case 0x01: // Text event
-									String eventText=readString(stream,metaLength);
+									String eventText=stream.readString(metaLength);
 									break;
 								case 0x02: // Copyright notice
-									String copyright=readString(stream,metaLength);
+									String copyright=stream.readString(metaLength);
 									license=copyright;
 									break;
 								case 0x03: // Sequence / track name
-									String trackName=readString(stream,metaLength);
+									String trackName=stream.readString(metaLength);
 									trackNames[i]=trackName;
 									break;
 								case 0x04: // Instrument name
-									String instrumentName=readString(stream,metaLength);
+									String instrumentName=stream.readString(metaLength);
 									break;
 								case 0x05: // Lyrics
-									String lyrics=readString(stream,metaLength);
+									String lyrics=stream.readString(metaLength);
 									break;
 								case 0x06: // Marker
 									// TODO Actually use markers?
-									String markerName=readString(stream,metaLength);
+									String markerName=stream.readString(metaLength);
 									break;
 								case 0x07: // Cue point
-									String cueName=readString(stream,metaLength);
+									String cueName=stream.readString(metaLength);
 									break;
 								case 0x20: // Midi channel prefix
 								case 0x21: // Midi port prefix
@@ -173,7 +169,7 @@ public class MidiParser implements SongFileParser
 									// TODO handle this
 									break;
 								case 0x51: // Set tempo
-									int microSecondsPerQuarterNote=readBytes(stream,3);
+									int microSecondsPerQuarterNote=stream.readBytes(3);
 									int microSecondsPerMinute=60000000;
 									bpm=microSecondsPerMinute/microSecondsPerQuarterNote;
 									break;
@@ -195,7 +191,7 @@ public class MidiParser implements SongFileParser
 									int scale=stream.read();
 									break;
 								case 0x7F: // Sequencer-specific event
-									String miscData=readString(stream,metaLength);
+									String miscData=stream.readString(metaLength);
 									break;
 								default:
 									throw new IOException("Unrecognized meta event code '"+Integer.toHexString(metaCode)+"' @ track "+(i+1)+" offset "+bytesRead);
@@ -203,10 +199,10 @@ public class MidiParser implements SongFileParser
 						}
 						else // manufacturer-exclusive event, aka we don't care
 						{
-							int metaLength=readVariableWidth(stream);
-							bytesRead+=lastVariableLength;
+							int metaLength=stream.readVariableWidth();
+							bytesRead+=stream.lastVariableLength();
 							// just read the proper number of bytes so we can all go home
-							readBytes(stream,metaLength);
+							stream.readBytes(metaLength);
 							bytesRead+=metaLength;
 						}
 						break;
@@ -229,51 +225,5 @@ public class MidiParser implements SongFileParser
 		
 		SongModel model=new SongModel(tracks,title,creators,email,website,license,bpm,null);
 		return model;
-	}
-	
-	/** Reads a variable-width value from a stream. */
-	// side note: variable-width values are reportedly never more than 4 bytes
-	// java int = 32-bit, so int should theoretically be fine for storage
-	private static int readVariableWidth(InputStream stream) throws IOException
-	{
-		int value=0;
-		int msig;
-		int res;
-		lastVariableLength=0;
-		
-		do
-		{
-			int b=stream.read();
-			msig=b & 0x80;
-			res=b & 0x7F;
-			value=value<<7;
-			value+=res;
-			lastVariableLength++;
-		} while (msig!=0);
-		
-		return value;
-	}
-	
-	/** Reads an n-byte value from the stream. */
-	private static int readBytes(InputStream stream,int num) throws IOException
-	{
-		int result=0;
-		for (int i=0; i<num; i++)
-		{
-			result=result<<8;
-			result+=stream.read();
-		}
-		return result;
-	}
-	
-	/** Reads a string of length n from the stream. */
-	private static String readString(InputStream stream,int n) throws IOException
-	{
-		StringBuilder res=new StringBuilder();
-		for (int i=0; i<n; i++)
-		{
-			res.append((char)stream.read());
-		}
-		return res.toString();
 	}
 }

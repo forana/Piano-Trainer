ae4890504ee7f7648be72cf14d09d78a9bd17beb
gartmannn@msoe.edu <gartmannn@msoe.edu>
2010-12-12 15:07:42 -0600

    case 2155 Added attach, detach, and update methods for the SongPlayer to allow things to properly interface with it

--- SongPlayer.java
+++ SongPlayer.java
@@ -1,5 +1,11 @@
 package crescendo.base;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import crescendo.base.song.SongModel;
 
 /**
@@ -10,12 +16,13 @@ import crescendo.base.song.SongModel;
 public class SongPlayer
 {
 	// TODO Pretty much all of this class
-	
+
 	/**
 	 * The songed that is being played.
 	 */
 	private SongModel songModel;
-	
+	private Map<NoteEventListener,Integer> listeners;
+	private List<NoteEvent> activeNotes;
 	/**
 	 * TODO This
 	 * 
@@ -24,13 +31,41 @@ public class SongPlayer
 	public SongPlayer(SongModel songModel)
 	{
 		this.songModel=songModel;
+
+		listeners = Collections.synchronizedMap(new HashMap<NoteEventListener,Integer>());
+		activeNotes = new ArrayList<NoteEvent>();
 	}
-	
+
 	/**
 	 * TODO This
 	 */
 	public SongModel getSong()
 	{
 		return this.songModel;
+	}
+
+	/**
+	 * Subscribe to receive note events from the song player
+	 * @param listener instance of NoteEventListener to pump events to
+	 * @param time number of milliseconds in advance the listener wants to receive note events
+	 */	
+	public void attach(NoteEventListener listener, int time) {
+		listeners.put(listener, time);
+	}
+
+	public void detach(NoteEventListener listener) {
+		listeners.remove(listener);
+	}
+
+
+	private void update() {
+		long now = System.currentTimeMillis();
+		for(NoteEvent event : activeNotes) {
+			for(NoteEventListener listener : listeners.keySet()) {
+				if(now >= (event.getTimestamp()-listeners.get(listener))) {
+					listener.handleNoteEvent(event);
+				}
+			}
+		}
 	}
 }

114c825e5e5810980e0b37f0878240338cb04bff
gartmannn@msoe.edu <gartmannn@msoe.edu>
2010-12-14 09:19:16 -0600

    case 2153 added the flow control structure and a subclass runnable to handle the internal timing of the song. Right now the frames per second is static, should we possibly make it have a minimum FPS and then increase with faster songs?

--- SongPlayer.java
+++ SongPlayer.java
@@ -3,9 +3,11 @@ package crescendo.base;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Iterator;
+import java.util.Set;
 
 import crescendo.base.song.SongModel;
 
@@ -13,38 +15,100 @@ import crescendo.base.song.SongModel;
  * A SongPlayer does the actual running through the song and propagation of events.
  * 
  * @author forana
+ * @author gartmannn
  */
 public class SongPlayer
 {
-	// TODO Pretty much all of this class
 
-	/**
-	 * The songed that is being played.
-	 */
+	
+	/** The song that is being played.*/
 	private SongModel songModel;
 	/** map of listeners and the number of milliseconds early to send their note events */
 	private Map<NoteEventListener,Integer> listeners;
 	/** list of notes which are currently in the queue */
-	private List<NoteEvent> activeNotes;
+	private Map<NoteEvent,List<NoteEventListener>> activeNotes;
+	
+	/** list of flow controllers (objects that get told about flow events such as pause and resume) */
+	private Set<FlowController> controllers;
+	/** Runnable which does timing */
+	private PlayerTimer timer;
+	/** Thread which holds our timer runnable */
+	private Thread timerContainer;
+	
+	private boolean isPaused;
+	private boolean doContinue;
+	
+	/** The number of millisections the song has been paused */
+	private long pauseOffset;
+	
 	/**
-	 * TODO This
-	 * 
-	 * @param songModel
+	 * Create a song player for the given song model. 
+	 * Initializes the lists of controllers and listeners, and creates the timer
+	 * There should be a 1 to 1 relationship between song players and song models
+	 * @param songModel song model representing the song we wish to play.
 	 */
 	public SongPlayer(SongModel songModel)
 	{
 		this.songModel=songModel;
-
 		listeners = Collections.synchronizedMap(new HashMap<NoteEventListener,Integer>());
-		activeNotes = new ArrayList<NoteEvent>();
+		controllers = Collections.synchronizedSet(new HashSet<FlowController>());
+		timer = new PlayerTimer();
+		timerContainer = new Thread(timer);
+	}
+	
+	/**
+	 * Begin playing the song
+	 * This resets all of the state variables, <b>DO NOT</b> resume the song after pausing, use the resume function. 
+	 */
+	public void play() {
+		activeNotes = new HashMap<NoteEvent,List<NoteEventListener>>();
+		isPaused = false;
+		doContinue = true;
+		pauseOffset = 0;
+		timerContainer.start();
+		//get a new iterator from the song model
+	}
+	
+	public void pause() {
+		isPaused = true;
+		for(FlowController controller : controllers) {
+			controller.pause();
+		}
+	}
+	
+	public void resume() {
+		isPaused = false;
+		for(NoteEvent event : activeNotes.keySet()) {
+			event.setTimestamp(event.getTimestamp()+pauseOffset);
+		}
+		pauseOffset = 0;
+		for(FlowController controller : controllers) {
+			controller.resume();
+		}
+	}
+	
+	public void stop() {
+		for(FlowController controller : controllers) {
+			controller.stop();
+		}
+		doContinue=false;
 	}
 
 	/**
-	 * TODO This
+	 * Get the song model for this song
+	 * @return the song model that this player is playing
 	 */
 	public SongModel getSong()
 	{
 		return this.songModel;
+	}
+
+	/** 
+	 * Subscribe to receive flow control calls from the song player
+	 * @param controller instance of FlowController to call on
+	 */
+	public void attach(FlowController controller) {
+		controllers.add(controller);
 	}
 
 	/**
@@ -64,6 +128,13 @@ public class SongPlayer
 		listeners.remove(listener);
 	}
 
+	/**
+	 * Remove controller from the controllers who are receiving control calls from the player
+	 * @param controller instacne of FlowController to remove from the list
+	 */
+	public void detach(FlowController controller) {
+		controllers.remove(controller);
+	}
 
 	/**
 	 * Sends out notes to the listeners. Only sends them out if it is
@@ -72,18 +143,61 @@ public class SongPlayer
 	 */
 	private void update() {
 		long now = System.currentTimeMillis();
+
+
+
+
+		//Pump out note events
 		NoteEvent event = null;
-		for(Iterator<NoteEvent> i = activeNotes.iterator(); i.hasNext();) {
+		for(Iterator<NoteEvent> i = activeNotes.keySet().iterator(); i.hasNext();) {
 			event = i.next();
 			if(now > event.getTimestamp())
 			{
-				i.remove();
+				i.remove(); //This removes the note from the map
 				continue;
 			}
 			for(NoteEventListener listener : listeners.keySet()) {
-				if(now >= (event.getTimestamp()-listeners.get(listener))) {
+				//Only send the note if it is within the correct range, and the listener has not received it yet
+				if(now >= (event.getTimestamp()-listeners.get(listener))
+						&& !activeNotes.get(event).contains(listener)) {
 					listener.handleNoteEvent(event);
+					activeNotes.get(event).add(listener);
 				}
+			}
+		}
+	}
+
+	/**
+	 * Runnable class which calls update FRAMES_PER_SECOND times per second
+	 * or adds to the pause offset if the song is paused.
+	 * @author nickgartmann
+	 */
+	private class PlayerTimer implements Runnable {
+		private final int FRAMES_PER_SECOND = 100;
+		/** number of milliseconds from the epoch of when the last frame started */
+		private long lastFrame = 0;
+		
+		/**
+		 * Method which get called by the thread, this is running while the song is playing or paused
+		 */
+		@Override
+		public void run() {
+			while(doContinue) {
+				long now = System.currentTimeMillis();
+				if(!isPaused) {
+					if(now > (lastFrame + (1000.0/(float)FRAMES_PER_SECOND))) {
+						update();			
+					}else{
+						try {
+							Thread.sleep(1); // Dont eat up all the processor
+							} 
+						catch (InterruptedException e) {}
+					}
+				}else{
+					pauseOffset+=(now-lastFrame);
+				}
+				lastFrame = now;	//We want to run at FRAMES_PER_SECOND fps, so use the beginning of the frame to
+									//ensure that we get the correct frames, no matter how long update takes
 			}
 		}
 	}

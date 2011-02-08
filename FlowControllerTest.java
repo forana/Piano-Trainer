c053ba65fdbc06128c05942809504b2ec49f9d0a
larkinp@msoe.edu <larkinp@msoe.edu>
2011-02-07 20:14:04 -0600

    finished major work on songselectionscreen, flowcontrollerbar, and dragmouseadapter, other changes.

--- FlowControllerTest.java
+++ FlowControllerTest.java
@@ -10,17 +10,17 @@ public class FlowControllerTest {
 	public static void main(String[] args) {
 		JFrame e = new JFrame();
 		e.setSize(800,600);
+		e.setLayout(null);
 		
+		DragMouseAdapter m = new DragMouseAdapter(800,505, null);
+		e.getContentPane().add(m);
 		
-		//DragMouseAdapter m = new DragMouseAdapter(800,600);
-		//e.getContentPane().add(m);
-		
-		//FlowControllerBar f = new FlowControllerBar();
+		FlowControllerBar f = new FlowControllerBar(0,505, 800,60, null);
 		//f.setSize(800,60);
 		//f.setLocation(0,50);
-		//e.add(f);
-		SongSelectionScreen s = new SongSelectionScreen(null,800,600);
-		e.getContentPane().add(s);
+		e.add(f);
+		//SongSelectionScreen s = new SongSelectionScreen(null,800,600);
+		//e.getContentPane().add(s);
 		e.setVisible(true);
 		
 	}

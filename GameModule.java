a6c6ecad8760e83fc336c26381857a22f2f5af0c
forana <none@none>
2011-04-01 10:44:21 -0500

    Repairing GameModule to a working state.

--- GameModule.java
+++ GameModule.java
@@ -1,17 +1,10 @@
 package crescendo.game;
 
-import java.awt.BorderLayout;
-
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-
-import crescendo.base.AudioPlayer;
-import crescendo.base.HeuristicsModel;
-import crescendo.base.SongPlayer;
-import crescendo.base.SongValidator;
-import crescendo.base.EventDispatcher.EventDispatcher;
+//import javax.swing.JComponent;
+//import crescendo.base.HeuristicsModel;
+//import crescendo.base.SongValidator;
 import crescendo.base.module.Module;
-import crescendo.base.profile.ProfileManager;
+//import crescendo.base.profile.ProfileManager;
 import crescendo.base.song.SongModel;
 import crescendo.base.song.Track;
 
@@ -20,16 +13,18 @@ public class GameModule extends Module {
 	
 	private String loadedSongPath;
 
-	private JComponent display;
-	private GameEngine gameEngine;
-	private SongValidator songValidator;
-
+	//private JComponent display;
+	//private GameEngine gameEngine;
+	//private SongValidator songValidator;
 	
-	public void showGamePanel(SongModel model, Track activeRef) {
+	public GameModule() {
+		this.showSongSelectionPanel();
+	}
+	
+	public void showGamePanel(SongModel model,Track activeTrack) {
 		this.removeAll();
 		this.add(new GameEngine(model,activeTrack));
-		songValidator = new SongValidator(model,activeRef,new HeuristicsModel(ProfileManager.getInstance().getActiveProfile().getIsPitchGraded(), ProfileManager.getInstance().getActiveProfile().getIsDynamicGraded()));
-
+		//songValidator = new SongValidator(model,activeTrack,new HeuristicsModel(ProfileManager.getInstance().getActiveProfile().getIsPitchGraded(), ProfileManager.getInstance().getActiveProfile().getIsDynamicGraded()));
 	}
 	
 	public void showSongSelectionPanel() {
@@ -44,50 +39,27 @@ public class GameModule extends Module {
 		this.updateUI();
 	}
 	
-	public void loadSong(SongModel model, int activeTrack){
-		
-		SongModel selectedSongModel = model;
-		
-		
-		//Hook up song processor pieces
-		EventDispatcher dispatcher = EventDispatcher.getInstance();
-			
-		gameEngine = new GameEngine(this,selectedSongModel);
-
-
-		display.add(gameEngine);
-
-
-		this.updateUI();
-		
-		
-		EventDispatcher.getInstance().registerComponent(gameEngine);
-	}
-	
 	public void play(){
-		gameEngine.play();
+		//gameEngine.play();
 	}
 	
 	public void pause(){
-		gameEngine.pause();
+		//gameEngine.pause();
 	}
 	
 	public void resume(){
-		gameEngine.resume();
+		//gameEngine.resume();
 	}
 	
 	public void stop(){
-		gameEngine.stop();
+		//gameEngine.stop();
 	}
 	
 	public void songEnd(){
-		gameEngine.songEnd();
-		songValidator.songEnd();
+		//gameEngine.songEnd();
+		//songValidator.songEnd();
 	}
 	
-
-
-
 	@Override
 	public String saveState() {
 		return loadedSongPath;

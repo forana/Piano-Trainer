030fa1c6ec72b43fcaae85aabf5ebf2c371d7bd8
forana <none@none>
2011-05-16 12:43:24 -0500

    Case 3985 Added margin to pages.

--- Chapter.java
+++ Chapter.java
@@ -7,12 +7,24 @@ import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.tree.TreeNode;
 
+/**
+ * Represents a single chapter in a JTree heirarchy.
+ * @author forana
+ */
 public class Chapter implements BookItem,LessonTreeNode
 {
+	// title of the chapter
 	private String title;
+	// inner lessons / chapters
 	private List<BookItem> contents;
+	// items for the chapter page itself
 	private List<PageItem> items;
 	
+	/**
+	 * @param title Title of the chapter
+	 * @param contents Inner lessons / chapters
+	 * @param items Items for the chapter page itself
+	 */
 	public Chapter(String title,List<BookItem> contents,List<PageItem> items)
 	{
 		this.title=title;
@@ -20,38 +32,63 @@ public class Chapter implements BookItem,LessonTreeNode
 		this.items=items;
 	}
 	
+	/**
+	 * @return All inner lessons / chapters belonging inside this chapter.
+	 */
 	public List<BookItem> getContents()
 	{
 		return this.contents;
 	}
 	
+	/**
+	 * @return The title of this chapter.
+	 */
 	public String getTitle()
 	{
 		return this.title;
 	}
 	
+	/**
+	 * @return The items on this lesson.
+	 */
 	public List<PageItem> getItems()
 	{
 		return this.items;
 	}
 	
+	/**
+	 * @param module The calling module.
+	 * @return A JPanel representing this chapter.
+	 */
 	public JPanel getPanel(JComponent module)
 	{
 		return new ChapterPanel(this,module);
 	}
 	
+	/**
+	 * @return The child node at the specified index.
+	 */
 	public TreeNode getChildAt(int childIndex) {
 		return this.contents.get(childIndex);
 	}
 	
+	/**
+	 * @return Whether or not this node can have children.
+	 */
 	public boolean isLeaf() {
 		return false;
 	}
 	
+	/**
+	 * @return How many children this node has.
+	 */
 	public int getChildCount() {
 		return this.contents.size();
 	}
 	
+	/**
+	 * @return The title of this chapter, with a score appended (if one can be calculated for this chapter).
+	 */
 	public String toString() {
 		String t=this.title;
 		int count=0;

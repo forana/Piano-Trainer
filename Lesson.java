cc53f04c20f08eb0ced3b65e301fd60842a57a36
forana <none@none>
2011-05-16 12:43:32 -0500

    Case 3985 Added margin to pages.

--- Lesson.java
+++ Lesson.java
@@ -7,36 +7,55 @@ import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.tree.TreeNode;
 
+/**
+ * Represents a single lesson in a JTree heirarchy.
+ * @author forana
+ */
 public class Lesson implements BookItem,LessonTreeNode
 {
+	// title of the lesson
 	private String title;
+	// page items
 	private List<PageItem> items;
 	
+	/**
+	 * @param title The title of the lesson.
+	 * @param items Items of which this lesson is comprised.
+	 */
 	public Lesson(String title,List<PageItem> items)
 	{
 		this.title=title;
 		this.items=items;
 	}
 	
+	/**
+	 * @return The title of the lesson.
+	 */
 	public String getTitle()
 	{
 		return this.title;
 	}
 	
+	/**
+	 * @return The items on this lesson.
+	 */
 	public List<PageItem> getItems()
 	{
 		return this.items;
 	}
 	
-	public void loadChildren()
-	{
-	}
-	
+	/**
+	 * @param module The calling module.
+	 * @return A JPanel representing this lesson.
+	 */
 	public JPanel getPanel(JComponent module)
 	{
 		return new LessonPanel(this,module);
 	}
 	
+	/**
+	 * @return The title of this lesson, with a score appended (if one can be calculated for this lesson).
+	 */
 	public String toString()
 	{
 		String t=this.title;
@@ -51,18 +70,30 @@ public class Lesson implements BookItem,LessonTreeNode
 		return t;
 	}
 	
+	/**
+	 * @return The child node at the specified index.
+	 */
 	public TreeNode getChildAt(int childIndex) {
 		return null;
 	}
 	
+	/**
+	 * @return Whether or not this node can have children.
+	 */
 	public boolean isLeaf() {
 		return true;
 	}
 	
+	/**
+	 * @return How many children this node has.
+	 */
 	public int getChildCount() {
 		return 0;
 	}
 	
+	/**
+	 * @return The number of MusicItems among this lesson's page items.
+	 */
 	public int getMusicItemCount() {
 		int count=0;
 		for (PageItem item : this.items) {
@@ -73,6 +104,9 @@ public class Lesson implements BookItem,LessonTreeNode
 		return count;
 	}
 	
+	/**
+	 * @return The total score (not averaged) for completed music items.
+	 */
 	public double getMusicItemTotal() {
 		double total=0;
 		for (PageItem item : this.items) {
@@ -87,6 +121,9 @@ public class Lesson implements BookItem,LessonTreeNode
 		return total;
 	}
 	
+	/**
+	 * @return The number of completed MusicItems among this lesson's page items.
+	 */
 	public int getMusicItemCompleteCount() {
 		int count=0;
 		for (PageItem item : this.items) {
